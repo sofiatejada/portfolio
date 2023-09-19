84e5001634a2974c12871b9ace4f12c60e06ee30
Sofia Tejada <79775413+sofiatejada@users.noreply.github.com>
2023-09-18 20:07:25 -0700

    i'm done

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable react/no-unescaped-entities */
 export default function Greeting() {
     return (
-        <section id="greeting">
-            <h1>hi.</h1>
-            <p>
-                dude hi, welcome! i code stuff and have varied interests...i
-                think i'm kinda quirky, but like a right amount. what do you
-                think?
-            </p>
+        <section
+            id="greeting"
+            className="flex pt-5 px-5 justify-around"
+        >
+            <section className="">
+                <h1 className=" font-bold text-6xl">hi.</h1>
+                <p className="">
+                    dude hi, welcome! i code stuff and have varied interests...i
+                    think i'm kinda quirky, but like a right amount. what do you
+                    think?
+                </p>
+            </section>
             <figure>
                 <img
                     src="/sofia.JPG"
                     alt="sofia with bunny ear filter"
+                    className="h-1/2"
                 />
             </figure>
         </section>
